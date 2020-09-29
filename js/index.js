b114d53b27edd62a9734cdaf2991fe70b977a4f3
baber-khan <baber97.bk@gmail.com>
2020-09-29 15:20:21 +0500

    Initial firebase commit

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
-window.alert('this is a dummy text')
+import * as firebaseConfig from "../firebaseConfig";
+
+var Config = {
+  apiKey: firebaseConfig.apiKey,
+  authDomain: firebaseConfig.authDomain,
+  databaseURL: firebaseConfig.databaseURL,
+  projectId: firebaseConfig.projectId,
+  storageBucket: firebaseConfig.storageBucket,
+  messagingSenderId: firebaseConfig.messagingSenderId,
+  appId: firebaseConfig.appId,
+  measurementId: firebaseConfig.measurementId,
+};
