24f780ef9745ae18bbc7fc0a0ab680cfb0892f16
baber-khan <baber97.bk@gmail.com>
2020-09-30 16:24:28 +0500

    Package.JSON

--- Main/Static/js/index.js
+++ Main/Static/js/index.js
@@ -1,2 +1,32 @@
+import {firebaseConfig} from "../../firebaseConfig";
+import app from 'firebase/app';
+import 'firebase/firestore';
 
-console.log("JS works");
+
+var Config = {
+  apiKey: firebaseConfig.apiKey,
+  authDomain: firebaseConfig.authDomain,
+  databaseURL: firebaseConfig.databaseURL,
+  projectId: firebaseConfig.projectId,
+  storageBucket: firebaseConfig.storageBucket,
+  messagingSenderId: firebaseConfig.messagingSenderId,
+  appId: firebaseConfig.appId,
+  measurementId: firebaseConfig.measurementId,
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+class firebase {
+
+    constructor(){
+        if(!app.apps.length){
+            app.initializeApp(Config);
+        }
+        this.firestore = app.firestore();
+    }
+
+    SaveWorkoutDate = document.getElementById("WorkoutDate").onclick = () => {
+        this.firestore.collection("workouts").add();
+    }
+}
