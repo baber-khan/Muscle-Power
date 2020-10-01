978eabb7eec0c0b0c72b06b4fa611f0e453d9228
baber-khan <baber97.bk@gmail.com>
2020-10-01 13:48:13 +0500

    Fixed Paths

---
+++ Main/config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBYx6MvDNG818Ff_Q1VKNo0f4qOPhTY-gU",
+    authDomain: "muscle-power.firebaseapp.com",
+    databaseURL: "https://muscle-power.firebaseio.com",
+    projectId: "muscle-power",
+    storageBucket: "muscle-power.appspot.com",
+    messagingSenderId: "486829713022",
+    appId: "1:486829713022:web:ac2c8c8defaee4b9b4da2f",
+    measurementId: "G-E89SMV0PBD"
+  };
