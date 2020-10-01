f86afe934b439a0fbeb84ae85cbf6d7c05170d30
baber-khan <baber97.bk@gmail.com>
2020-10-01 13:49:10 +0500

    Seprated Firebase and JS Functions

--- Main/Static/js/index.js
+++ Main/Static/js/index.js
@@ -1,32 +1,4 @@
-import {firebaseConfig} from "../../firebaseConfig";
-import app from 'firebase/app';
-import 'firebase/firestore';
+// import {firebaseHelper} from './firebase';
 
 
-var Config = {
-  apiKey: firebaseConfig.apiKey,
-  authDomain: firebaseConfig.authDomain,
-  databaseURL: firebaseConfig.databaseURL,
-  projectId: firebaseConfig.projectId,
-  storageBucket: firebaseConfig.storageBucket,
-  messagingSenderId: firebaseConfig.messagingSenderId,
-  appId: firebaseConfig.appId,
-  measurementId: firebaseConfig.measurementId,
-};
-
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
-
-class firebase {
-
-    constructor(){
-        if(!app.apps.length){
-            app.initializeApp(Config);
-        }
-        this.firestore = app.firestore();
-    }
-
-    SaveWorkoutDate = () => {
-        this.firestore.collection("workouts").add();
-    }
-}
+document.getElementById("WorkoutDate").addEventListener("click", () => {console.log("This works");})
