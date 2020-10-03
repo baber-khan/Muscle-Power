6a2430440f4f1c9217109e43353a1526905be5f5
baber-khan <baber97.bk@gmail.com>
2020-10-03 18:06:35 +0500

    Added new Interfaces

--- Main/interfaces.ts
+++ Main/interfaces.ts
@@ -1,13 +1,35 @@
-export interface firebaseAuthUser {
+export interface FirebaseAuthUser {
     uid: string,
-    name: string,
-    Age: number,
+    displayName: string
+}
+
+export interface DBUser {
+    user: FirebaseAuthUser
     weight: number,
     height: string,
+    age: number,
     gender: string
 }
 
-export interface excercise {
+export interface Excercise {
     id: string,
     title: string
 }
+
+export interface WorkoutDay {
+    day: string,
+    excersices: Array<Excercise>
+    user: FirebaseAuthUser
+}
+
+export interface Meal {
+    id: string,
+    title: string
+}
+
+export interface FoodDay {
+    meals: Array<Meal>,
+    day: string,
+    user: FirebaseAuthUser
+}
+
