479acf3526c5b3f753ad430f11117765a966b5f7
baber-khan <baber97.bk@gmail.com>
2020-10-03 16:00:47 +0500

    Service Worker and Interface Setup

---
+++ Main/serviceWorker.ts
@@ -0,0 +1,12 @@
+import { excercise } from "./interfaces";
+
+export const excercises: Array<excercise> = [
+    {
+        id: 'push-up',
+        title: 'Push Up'
+    },
+    {
+        id: 'chin-up',
+        title: 'Chin Up'
+    }
+]
