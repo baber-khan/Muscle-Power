352d3076152f1619cf2553b4230514936b3fce26
ibrahim-jamil <ibrahimbhai671@gmail.com>
2020-10-04 22:32:58 +0500

    Added Exercises ID And Titles

--- Main/serviceWorker.ts
+++ Main/serviceWorker.ts
@@ -1,12 +1,343 @@
 import { excercise } from "./interfaces";
 
 export const excercises: Array<excercise> = [
-    {
-        id: 'push-up',
-        title: 'Push Up'
-    },
-    {
-        id: 'chin-up',
-        title: 'Chin Up'
-    }
-]
+  {
+    id: "push-up",
+    title: "Push Up",
+  },
+
+  {
+    id: "chin-up",
+    title: "Chin Up",
+  },
+  // (Arms Exercises)
+  //Bicep Exercises
+  //{ website url: https://manofmany.com/lifestyle/fitness/best-bicep-exercises-for-men}
+  {
+    id: "alternating-incline-dumbbell-curl",
+    title: "Alternating Incline Dumbbell Curl",
+  },
+  {
+    id: "seated-alternating-hammer-curl",
+    title: "Seated Alternating Hammer Curl",
+  },
+  {
+    id: "standing-reverse-barbell-curl",
+    title: "Standing Reverse Barbell Curl",
+  },
+  {
+    id: "seated-alternating-dumbbell-curl",
+    title: "Seated Alternating Dumbbell Curl",
+  },
+  {
+    id: "standing-cable-curl",
+    title: "Standing Cable Curl",
+  },
+  {
+    id: "standing-barbell-curl",
+    title: "Standing Barbell Curl",
+  },
+  {
+    id: "decline-dumbbell-curl",
+    title: "Decline Dumbbell Curl",
+  },
+  {
+    id: "concentration-curl",
+    title: "Concentration Curl",
+  },
+  {
+    id: "cable-flex-curl",
+    title: "Cable Flex Curl",
+  },
+  //Triceps Exercises
+  //{website:https://www.prevention.com/fitness/workouts/g27667203/best-triceps-exercises/}
+  {
+    id: "overhead-triceps-extension",
+    title: "Overhead Triceps Extension",
+  },
+  {
+    id: "triceps-kickbacks",
+    title: "Triceps Kickbacks",
+  },
+  {
+    id: "triceps-underhand-kickbacks",
+    title: "Triceps Underhand Kickbacks",
+  },
+  {
+    id: "standing-eccentric-triceps-extensions",
+    title: "Standing Eccentric Triceps Extensions",
+  },
+  {
+    id: "skull-crushers",
+    title: "Skull Crushers",
+  },
+  {
+    id: "close-grip-dumbbell-press",
+    title: "Close-Grip Dumbbell Press",
+  },
+  {
+    id: "tate-press",
+    title: "Tate Press",
+  },
+  {
+    id: "diamond-push-ups",
+    title: "Diamond Push-Ups",
+  },
+  {
+    id: "chaturanga-push-up",
+    title: "Chaturanga Push-Up",
+  },
+  {
+    id: "up-down-planks",
+    title: "Up-Down Planks",
+  },
+  {
+    id: "chair-dips",
+    title: "Chair Dips",
+  },
+  //Forearms Exercises
+  //{website : https://www.mensjournal.com/health-fitness/the-20-best-forearms-exercises-of-all-time/}
+  {
+    id: "behind-the-back-cable-curl",
+    title: "Behind-the-Back Cable Curl",
+  },
+  {
+    id: "ez-bar-preacher-curl",
+    title: "EZ-Bar Preacher Curl",
+  },
+  {
+    id: "reverse-curl",
+    title: "Reverse Curl",
+  },
+  {
+    id: "wide-grip-curl",
+    title: "Wide-Grip Curl",
+  },
+  {
+    id: "towel-cable-row",
+    title: "Towel Cable Row",
+  },
+  {
+    id: "towel-pullup",
+    title: "Towel Pullup",
+  },
+  {
+    id: "towel-row-to-chest",
+    title: "Towel Row-to-Chest",
+  },
+  {
+    id: "wrist-curl",
+    title: "Wrist Curl",
+  },
+  {
+    id: "chinup",
+    title: "Chinup",
+  },
+  {
+    id: "parallel-bar-hand-walk",
+    title: "Parallel Bar Hand Walk",
+  },
+  //Shoulder Exercises
+  //{website: https://www.mensjournal.com/health-fitness/30-best-shoulder-exercises-and-workouts-of-all-time/}
+  {
+    id: "barbell-overhead-press",
+    title: "Barbell Overhead Press",
+  },
+  {
+    id: "standing-dumbbell-fly",
+    title: "Standing Dumbbell Fly",
+  },
+  {
+    id: " face-pull",
+    title: "Face Pull",
+  },
+  {
+    id: "high-pull",
+    title: "High Pull",
+  },
+  {
+    id: "seated-dumbbell-clean",
+    title: "Seated Dumbbell Clean",
+  },
+  {
+    id: "trap-raise",
+    title: "Trap Raise",
+  },
+  {
+    id: "clean-and-press",
+    title: "Clean and Press",
+  },
+  {
+    id: "hindu-pushup",
+    title: "Hindu Pushup",
+  },
+  {
+    id: "dip",
+    title: "Dip",
+  },
+  {
+    id: "rack-deadlift",
+    title: "Rack Deadlift",
+  },
+  //Upper Chest Exercises
+  //{website: https://thefitnesstribe.com/upper-chest-exercises/}
+
+  {
+    id: "decline-push-up",
+    title: "Decline Push-Up",
+  },
+  {
+    id: "incline-bench-press",
+    title: "Incline Bench Press",
+  },
+  {
+    id: "leverage-incline-chest-press",
+    title: "Leverage Incline Chest Press",
+  },
+  {
+    id: "chest-dips",
+    title: "Chest Dips",
+  },
+  {
+    id: "the-chest-fly",
+    title: "The Chest Fly",
+  },
+  {
+    id: "plyometric-push-up",
+    title: "Plyometric Push-Up",
+  },
+  {
+    id: "reverse-grip-bench-press",
+    title: "Reverse Grip Bench Press",
+  },
+  {
+    id: "dumbbell-pullover",
+    title: "Dumbbell Pullover",
+  },
+  //lower chest exercises
+  //{website: https://www.medicalnewstoday.com/articles/324490}
+  {
+    id: "incline-pushup",
+    title: "Incline pushup",
+  },
+  {
+    id: "decline-dumbbell-press",
+    title: "Decline dumbbell press",
+  },
+  {
+    id: " decline-dumbbell-bench-press-with-external-rotation",
+    title: " Decline dumbbell bench press with external rotation",
+  },
+  {
+    id: "cable-crossover",
+    title: "Cable Crossover",
+  },
+  {
+    id: "parallel-bar-dips",
+    title: "Parallel-Bar Dips",
+  },
+  ///(back)
+  //Upper Back Exercises
+  //{website: https://www.healthline.com/health/fitness-exercise/upper-back-pain-exercises}
+
+  {
+    id: "neck-side-bend-and-rotation",
+    title: "Neck side bend and rotation",
+  },
+  {
+    id: "overhead-arm-reach",
+    title: "Overhead Arm Reach",
+  },
+  {
+    id: "childs-pose",
+    title: "Child’s Pose",
+  },
+  {
+    id: "knee-to-chest",
+    title: "Knee To Chest",
+  },
+  //lower back Exercises
+  //{website: https://www.medicalnewstoday.com/articles/323204}
+  {
+    id: "bridges",
+    title: "Bridges",
+  },
+  {
+    id: "knee-to-chest-stretches",
+    title: "Knee-To-Chest Stretches",
+  },
+  {
+    id: "lower-back-rotational-stretches",
+    title: "Lower Back Rotational Stretches",
+  },
+  {
+    id: "lying-lateral-leg-lifts",
+    title: "Lying Lateral Leg Lifts",
+  },
+  {
+    id: "cat-stretches",
+    title: "Cat Stretches",
+  },
+  {
+    id: "partial-curls",
+    title: "Partial Curls",
+  },
+  //Exercises for Building Badass Wings
+  //{website: https://www.muscleandperformance.com/training-performance/3-best-exercises-for-building-badass-wings-5929}
+  {
+    id: "wide-grip-pull-up",
+    title: "Wide-Grip Pull-Up",
+  },
+  {
+    id: "bent-over-barbell-row",
+    title: "Bent-Over Barbell Row",
+  },
+  {
+    id: "straight-arm-lat-pull-down",
+    title: "Straight-Arm Lat Pull-Down",
+  },
+  //Thai Exercises
+  //{website:https://www.onefc.com/lifestyle/5-muay-thai-exercises-you-can-do-at-home-while-life-is-on-pause/}
+  {
+    id: "skipping-rope",
+    title: "Skipping Rope",
+  },
+  {
+    id: "sit-ups",
+    title: "Sit-Ups",
+  },
+  {
+    id: "pull-ups",
+    title: "Pull-Ups",
+  },
+  // Calf Exercises
+  //{Website: https://manofmany.com/lifestyle/fitness/best-calf-exercises-for-men}
+  {
+    id: "box-jumps",
+    title: "Box Jumps",
+  },
+  {
+    id: "jumping-jacks",
+    title: "Jumping Jacks",
+  },
+  {
+    id: "seal-jumps",
+    title: "Seal Jumps",
+  },
+  {
+    id: "agility-ladder",
+    title: "Agility Ladder",
+  },
+  {
+    id: "seated-band-pushes",
+    title: "Seated Band Pushes",
+  },
+  {
+    id: "explosive-calf-step-up",
+    title: "Explosive Calf Step-Up",
+  },
+  {
+    id: "hill-runs",
+    title: "Hill Runs",
+  },
+];
