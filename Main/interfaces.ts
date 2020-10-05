fe4efd3bcf8945b36136d497495c0fc7ea688711
baber-khan <baber97.bk@gmail.com>
2020-10-05 10:25:31 +0500

    Fixed interfaces

--- Main/interfaces.ts
+++ Main/interfaces.ts
@@ -3,8 +3,7 @@ export interface FirebaseAuthUser {
     displayName: string
 }
 
-export interface DBUser {
-    user: FirebaseAuthUser
+export interface DBUser extends FirebaseAuthUser {
     weight: number,
     height: string,
     age: number,
@@ -16,15 +15,23 @@ export interface Excercise {
     title: string
 }
 
+export interface CustomDate {
+    day: number;
+    month: number;
+    year: number;
+}
+
 export interface WorkoutDay {
-    day: string,
+    // date: string, // 12/09/2020 12-09-2020
+
     excersices: Array<Excercise>
     user: FirebaseAuthUser
 }
 
 export interface Meal {
     id: string,
-    title: string
+    title: string,
+    timeOfDay: 'breakfast' | 'lunch' | 'dinner'; 
 }
 
 export interface FoodDay {
