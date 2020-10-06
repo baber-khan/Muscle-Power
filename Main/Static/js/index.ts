0d16cf9664402161bc9ddaf47d624c3b28e1af54
baber-khan <baber97.bk@gmail.com>
2020-10-06 11:40:30 +0500

    Test Function

--- Main/Static/js/index.ts
+++ Main/Static/js/index.ts
@@ -1,6 +1,11 @@
 import { firebaseHelper } from './firebase';
+import { excercises } from '../../serviceWorker';
 
 document.getElementById('WorkoutDateButton').addEventListener('click', () => {
   const Date = document.getElementById('WorkoutDate') as HTMLInputElement;
   firebaseHelper.saveWorkoutDate(Date.value);
+});
+
+excercises.forEach((value) => {
+  console.log(value.id);
 });
